40c59fe49c1d6cd55cb9e8c9dbd9f4f512ce1d70
Yauheni Mikhno <jenia.mikhno@gmail.com>
2023-09-18 15:04:58 +0200

    changed JavaScript

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ console.log(
 
 const heading = document.querySelector(".navigation");
 
-heading.innerHTML =
-  "`Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age} lat.`";
+//*heading.innerHTML =
+ //* "`Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age} lat.`"; */
 
 const emptyParagraph = document.querySelector(".day-summary__description--js");
 
@@ -27,9 +27,57 @@ function greet(age, firstName) {
   );
 }
 
+const humanOne = {
+  name: 'Maciek',
+  age:32,
+  address: {
+    street:'Warszawska',
+    city: 'Białystok'
+  }
+}
+
+const humanTwo = {
+  name: 'Stefan',
+  age: 35,
+  adress: humanOne.address
+}
+
 function createContent(querySelectorContent, content) {
   const element = document.querySelector(querySelectorContent);
   element.innerHTML = content;
 }
 
 createContent('.day-summary__description--js', 'Siema siema');
+
+console.log(typeof 2);
+console.log(typeof '2');
+
+if (2=='2') {
+  console.log('Java to nie JavaScript')
+}
+if (!('JavaScript' == 'Java')) {
+  console.log('to sie nie wykona')
+}
+if (humanOne.age > humanTwo.age) {
+  console.log('Human one jest starszy')
+} else if (humanOne.age=== humanTwo.age){
+  console.log('są równolatkami')
+}
+else {
+  console.log('Human two jest starszy')
+}
+const myNumber = {name: 'Maciek'}
+
+switch (myNumber) {
+  case 7:
+    console.log('jestem siódemką');
+  break;
+  case 9:
+    console.log('jestem dziewiątką');
+  break;
+  default:
+    console.log('jestem czymś innym')
+    console.log(myNumber)  
+}
+
+(32>20) ? console.log('to prawda'): console.log('to nieprawda');
